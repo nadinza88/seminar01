fdd38bc73672536181ff5db8af282e1f44869242
nadinza88 <nadimihalenko@gmail.com>
2022-11-16 17:39:35 +0300

    Seminar02

---
+++ seminar02/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine ("Введите первое число");
+
+string inputA = Console.ReadLine ();
+
+int numberA = int.Parse (inputA);
+
+Console.WriteLine ("Введите второе число");
+
+string inputB = Console.ReadLine ();
+
+int numberB = int.Parse (inputB);
+
+if (numberA>numberB)
+
+{Console.WriteLine ($"{numberA} - max; {numberB} - min");
+} 
+
+else {
+    Console.WriteLine ($"{numberA} - min; {numberB} - max");
+}
